d539404e9232189a82ac25ef4ca142af53a70c3c
Maxim Khotsevich <mkhotsevich@gmail.com>
2022-05-25 00:03:46 +0300

    Создал файл для диплоя

---
+++ ecosystem.config.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  apps: [
+    {
+      name: 'collabswithme',
+      script: './dist/main.js',
+      cwd: '/var/www',
+      watch: false,
+      env_development: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'root',
+      host: ['109.70.24.73'],
+      ref: 'origin/main',
+      repo: 'git@github.com:mkhotsevich/collaber-backend.git',
+      path: '/var/www/collabswithme',
+      'post-deploy':
+        'npm install && npm run build && pm2 startOrRestart ecosystem.config.cjs --env production',
+    },
+    development: {
+      user: 'root',
+      host: ['109.70.24.73'],
+      ref: 'origin/main',
+      repo: 'git@github.com:mkhotsevich/collaber-backend.git',
+      path: '/var/www/collabswithme',
+      'post-deploy':
+        'npm install && npm run build && pm2 startOrRestart ecosystem.config.cjs --env development',
+    },
+  },
+};
