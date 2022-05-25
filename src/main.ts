554d73dbb341ee48ab72526ef216834939333d9d
Maxim Khotsevich <mkhotsevich@sberbank.ru>
2022-05-25 16:50:53 +0300

    Поправил путь

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
 
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-    logger: ['debug'],
-  });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
